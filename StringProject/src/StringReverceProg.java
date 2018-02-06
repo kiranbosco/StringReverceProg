7668067510527dd36e299816c8949a613ea18c4e
Superman computers <Superman computers@DESKTOP-AGHAT19>
2018-02-06 23:50:59 +0530

    String Rev class

---
+++ StringProject/src/StringReverceProg.java
@@ -0,0 +1,13 @@
+
+public class StringReverceProg {
+
+	public static void main(String[] args) {
+		
+		String rev=new StringBuilder("inirup").reverse().toString();
+		System.out.println(rev);
+	
+		
+	
+	}
+}
+
